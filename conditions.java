dccba800ffd479d951fd752c0235bf7b8ae25ca7
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-13 14:45:04 +0530

    condition01

---
+++ conditions.java
@@ -0,0 +1,14 @@
+public class conditions{
+    public static void main(String[] args) {
+        int age = 23;
+
+        System.out.println("Age = "+age);
+
+        if(age>=18){
+            System.out.println("you are eligibe to vote.");
+        }
+        else{
+            System.out.println("you aren't eligible to vote.");
+        }
+    }
+}
