a515a82738f09c0bb1190825202b02d0eab23efc
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-13 12:42:18 +0530

    logic

---
+++ logic.java
@@ -0,0 +1,13 @@
+public class logic{
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 4;
+
+       
+        System.out.println("a && b: " + (a && b));
+        System.out.println("a || b: " + (a || b));
+    }
+}
+
+     
