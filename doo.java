9a033e13f52ba35ccc1fb005e9c0ab70ccae56fe
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 11:01:14 +0530

    print5times

---
+++ doo.java
@@ -0,0 +1,13 @@
+public class doo 
+{     
+    public static void main(String[] args)      
+    {         
+        int i = 1;          
+        do         
+        {             
+            System.out.println("avacado");            
+        i++;         
+        }             
+            while(i<=5);                  
+    } 
+}
