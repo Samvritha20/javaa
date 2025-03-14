77209c766b2ee9032fc0e0e4992c275ef38b53ce
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 13:20:50 +0530

    methods with arithmetics

---
+++ methods.java
@@ -0,0 +1,19 @@
+public class methods
+{
+    static void mymethod()
+    {
+        int a=1;
+        int b=3;
+        int c=a*b;
+        int d=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("Sum : "+d);
+        System.out.println("Product : "+c);
+
+    }
+    public static void main(String[] args)
+    {
+        mymethod();
+    }
+}
