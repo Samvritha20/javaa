47ad6a8f82dda3074ce17158d581d0c36606df1d
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 15:48:02 +0530

    Inheritance

---
+++ classs.java
@@ -0,0 +1,16 @@
+
+     class Student { 
+        int id;
+        String name;
+    }
+
+    public  class classs {
+        public static void main(String[]args) {
+            Student s1 = new Student();
+            s1.id = 101;
+            s1.name = "samantha";
+            System.out.println(s1.name);
+            System.out.println(s1.id);
+        }
+    }
+
