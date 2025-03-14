4758a6416987b01b38d81df04d99c9ea0e4d17ec
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 10:41:01 +0530

    whiletwo

---
+++ whilelotwo.java
@@ -0,0 +1,10 @@
+public class whilelotwo {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i<=5) {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
