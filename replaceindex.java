d391e3de625502d9ffb68a3a14190bf0732357b6
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 14:40:09 +0530

    replacevalueinarray

---
+++ replaceindex.java
@@ -0,0 +1,15 @@
+public class replaceindex
+{
+    public static void main(String[] args)
+    {
+        String[] fruits = {"Avocado ", "Pineapple ", "DragonFruits "};
+
+
+        fruits[0] = "Orange ";
+
+
+        System.out.print(fruits[0]);
+        System.out.print(fruits[1]);
+        System.out.print(fruits[2]);
+    } 
+}
