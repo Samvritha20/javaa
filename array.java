7222778f045c787c51944809c455bc8f16dd6c2b
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 14:03:07 +0530

    indexvalueofarray

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array
+{
+    public static void main(String[] args)
+    {
+        String[] fruits = {"Avocado","Pineapple","DragonFruits"};
+
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    } 
+}
