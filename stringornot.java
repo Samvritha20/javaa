16d6766c1310372b490197ca50beaa4e3a3e3e34
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 11:15:48 +0530

    ifstringornot

---
+++ stringornot.java
@@ -0,0 +1,11 @@
+public class stringornot {
+    public static void main(String[] args) {
+        String a = "He like avocado";
+        
+        if (a.contains("like")) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
