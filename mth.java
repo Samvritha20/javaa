f3b57666a0d6d3e9f49f717452fa207af83fab54
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 12:18:37 +0530

    samplemethodcreation

---
+++ mth.java
@@ -0,0 +1,11 @@
+public class mth
+{
+    static void mymethod()
+    {
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args)
+    {
+        mymethod();
+    }
+}
