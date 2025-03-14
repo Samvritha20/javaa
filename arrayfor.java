d40c852808a0067d40240e6f3a33604392dab569
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-14 15:26:23 +0530

    index using for loop

---
+++ arrayfor.java
@@ -0,0 +1,12 @@
+public class arrayfor
+{
+    public static void main(String[] args)
+    {
+        String[] fruits = {"Avocado ", "Pineapple ", "DragonFruits ", "Apple ", "Orange ", "Kiwi "};
+        
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.println(fruits[i]); 
+        }
+    }
+}
