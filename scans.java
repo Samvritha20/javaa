b37f295a40da4704ea377941338b29d4bd6a0ddf
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-15 11:55:41 +0530

    Scanner class

---
+++ scans.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class scans
+{
+    public static void main(String[] args)
+    {
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Enter the Username : ");
+        String username = myobj.next();
+        System.out.println("The username is "+username);
+
+    }
+}
