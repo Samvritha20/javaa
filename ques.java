b5b31bad6a2eaa9660da65778fa5586be76c767d
Samvritha20 <samvritha2004outlook.com@gmail.com>
2025-03-15 10:39:48 +0530

    Answer:Question 1 with array & for loop cocept

---
+++ ques.java
@@ -0,0 +1,14 @@
+public class ques
+{
+    public static void main(String[] args)
+    {
+        int[] invonumm = {01,02,03,04,05,06,07};
+
+        System.out.println("The invoice number of first five customer are :");
+
+        for(int i=0; i<=5; i++)
+        {
+             System.out.println( invonumm[i] );
+        }
+    }
+}
